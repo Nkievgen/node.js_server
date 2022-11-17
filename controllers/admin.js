7e4e3d39ecd8c58c97846972079e34e02c436485
Nkievgen <nkievgen@gmail.com>
2022-11-17 05:49:41 -0500

    updated controllers and routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,46 +1,28 @@
-//auth flow is not yet implemented so admin controllers do not check if user is an admin
-
 const mongodb = require('mongodb');
 const Product = require('../models/product');
 const user = require('../models/user');
 
-//controller fetches all products in db and then renders product page
+//fetching all products from the db and rendering product list page
 exports.getProductList = (req, res, next) => {
-    console.log('handling get admin product list request');
-    let userId;
-    let userName;
-    if (req.session.user) {
-        userId = req.session.user._id;
-        userName = req.session.user.name;
-    } else {
-        res.redirect('/login');
-    }
     Product
         .find()
         .then(products => {
-            console.log('sending admin productlist page', '\n');
             res.render('./admin/product-list',{
                 prods: products,
                 pageTitle: "Admin Product List",
-                path: "/admin/product-list",
-                userId: userId,
-                userName: userName
+                path: "/admin/product-list"
             });
         })
-        .catch(
-            err => console.log(err)
-        );
+        .catch(err => {
+            console.log(err);
+            req.flash('error', 'Unexpected error');
+            res.redirect('/');
+        });
 }
 
-//controller reads a post request and adds a new product to db, redirects to add product controller
+//adding a new product to the db, redirecting to add product
 exports.postAddProduct = (req, res, next) => {
-    console.log('handling post add product request');
-    let userId;
-    if (req.session.user) {
-        userId = req.session.user._id;
-    } else {
-        res.redirect('/login');
-    }
+    const userId = req.session.user._id;
     const title = req.body.title;
     const price = req.body.price;
     const imageUrl = req.body.imageUrl;
@@ -55,51 +37,31 @@ exports.postAddProduct = (req, res, next) => {
     product
         .save()
         .then(() => {
-            console.log('product created');
             res.redirect('/admin/add-product');
         })
         .catch(err => {
             console.log(err);
+            req.flash('error', 'Unexpected error');
+            res.redirect('/');
         });
 }
 
-//controllers renders add product page
+//rendering add product page
 exports.getAddProduct = (req, res, next) => {
-    console.log('handling get add product request');
-    console.log('sending add product page', '\n');
-    let userId;
-    let userName;
-    if (req.session.user) {
-        userId = req.session.user._id;
-        userName = req.session.user.name;
-    } else {
-        res.redirect('/login');
-    }
     res.render('./admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false,
-        userId: userId,
-        userName: userName
+        editing: false
     })
 }
 
-//controller fetches product data and then passes it to edit page to pre-populate input fields in edit product page
+//fetching product data and then rendering edit page with pre-populated input fields
 exports.getEditProduct = (req, res, next) => {
-    console.log('handling get edit product request');
     const editMode = req.query.edit;
     if (!editMode) {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    let userId;
-    let userName;
-    if (req.session.user) {
-        userId = req.session.user._id;
-        userName = req.session.user.name;
-    } else {
-        res.redirect('/login');
-    }
     Product
         .findById(prodId)
         .then((product) => {
@@ -110,22 +72,18 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product,
-                userId: userId,
-                userName: userName
+                product: product
             })
         })
-        .catch(err => 
-            console.log(err)
-        );
+        .catch(err => {
+            console.log(err);
+            req.flash('error', 'Unexpected error');
+            res.redirect('/');
+        });
 }
 
-//controller reads a request and  updates a product in db, redirects to product list controller
+//updating a product in the db, redirecting to product list
 exports.postEditProduct = (req, res, next) => {
-    console.log('handling post edit product request');
-    if (!req.session.user) {
-        res.redirect('/login');
-    }
     const productId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
@@ -144,26 +102,24 @@ exports.postEditProduct = (req, res, next) => {
             console.log('product updated');
             res.redirect('/admin/product-list');
         })
-        .catch(err =>
-            console.log(err)
-        );
+        .catch(err => {
+            console.log(err);
+            req.flash('error', 'Unexpected error');
+            res.redirect('/');
+        });
 }
 
-//controlelr deletes a product from a db and redirects to product list controller
+//deleting a product from the db and redirecting to product list
 exports.postDeleteProduct = (req, res, next) => {
-    console.log('handling post delete product request');
-    if (!req.session.user) {
-        res.redirect('/login');
-    }
     const productId = req.body.productId;
     Product
         .findByIdAndRemove(productId)
         .then(() => {
-            console.log('product deleted');
-            console.log('redirecting to admin product list');
             res.redirect('/admin/product-list');
         })
-        .catch(err =>
-            console.log(err)
-        );
+        .catch(err => {
+            console.log(err);
+            req.flash('error', 'Unexpected error');
+            res.redirect('/');
+        });
 }
