7abdb67dac0fcca52d2ce5a8b2a94bee5d3fe0cf
Nkievgen <nkievgen@gmail.com>
2022-11-17 05:47:48 -0500

    csrf, flash and middleware for setting locals

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const path = require('path');
 
+//npm modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const mongoDbStore = require('connect-mongodb-session')(session);
+const csurf = require('csurf');
+const flash = require('connect-flash');
 
+//file with mongodb connection link
 const MONGODB_URI = require('./mongodburi');
 
+//routes importting
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorRoutes = require('./routes/errors');
 const authRoutes = require('./routes/auth');
 
-const User = require('./models/user');
-
 const app = express();
+
+//session storage
 const store = new mongoDbStore({
     uri: MONGODB_URI,
     collection: 'sessions'
 });
 
+const csrfProtection = csurf();
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//session initialization
 app.use(
     session({
         secret: 'secret placeholder',
@@ -33,29 +44,49 @@ app.use(
     })
 );
 
+app.use(flash());
+
+app.use(csrfProtection);
+
+//setting userId, userName and messages in locals
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    if (req.session.user) {
+        res.locals.userId = req.session.user._id;
+        res.locals.userName = req.session.user.name;
+    } else {
+        res.locals.userId = false;
+        res.locals.userName = false;
+    }
+    let message = req.flash('message');
+    let errorMessage = req.flash('error');
+    if (message.length > 0) {
+        res.locals.message = message[0];
+    } else {
+        res.locals.message = false;
+    }
+    if (errorMessage.length > 0) {
+        res.locals.errorMessage = errorMessage[0];
+    } else {
+        res.locals.errorMessage = false;
+    }
+    next();
+})
+
+//routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 app.use(errorRoutes);
 
-//connects to the database and then launches server if successfull
+//connecting to the database and starting to listen if successfull
 mongoose
-.connect(
-    MONGODB_URI
-)
-.then(() => {
-    console.log('mongodb connection successfull');
-    // const newUser = new User({
-    //     name: 'User2',
-    //     email: 'user2@test.com',
-    //     cart: {
-    //         items: []
-    //     }
-    // })
-    // newUser.save();
-    app.listen(3000);
-    console.log('listening...');
-})
-.catch(err => {
-    console.log(err);
-})
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('mongodb connection successfull');
+        app.listen(3000);
+        console.log('listening...');
+    })
+    .catch(err => {
+        console.log(err);
+    })
